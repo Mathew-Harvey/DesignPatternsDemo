c66bf3ae52ba7ce2707df3e9d3af1740f24d9771
mathew harvey <mathewharvey@gmail.com>
2023-11-11 21:13:41 +0800

    adding animation to printer queue

--- FrontEnd/js/singleton.js
+++ FrontEnd/js/singleton.js
@@ -60,6 +60,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const deskImgThree = new Image();
     const printerImg = new Image();
     const documentImg = new Image();
+    const personOne = new Image();
+    const personTwo = new Image();
+    const personThree = new Image();
+    const compOne = new Image();
+    const compTwo = new Image();
+    const compThree = new Image();
+    const coffeeCup = new Image();
+
 
     // Set the source for your images
     backgroundFloor.src = '../assetsSinglton/floor.png';
@@ -68,6 +76,13 @@ document.addEventListener("DOMContentLoaded", function () {
     deskImgThree.src = '../assetsSinglton/deskThree.png';
     printerImg.src = '../assetsSinglton/printer.png';
     documentImg.src = '../assetsSinglton/document.png';
+    personOne.src = '../assetsSinglton/personThree.png';
+    personTwo.src = '../assetsSinglton/personOne.png';
+    personThree.src = '../assetsSinglton/personTwo.png';
+    compOne.src = '../assetsSinglton/compTwo.png';
+    compTwo.src = '../assetsSinglton/compThree.png';
+    compThree.src = '../assetsSinglton/compOne.png';
+    coffeeCup.src = '../assetsSinglton/coffeeCup.png';
 
     // Promises to track the loading of each image
     const loadFloor = new Promise((resolve) => { backgroundFloor.onload = resolve; });
@@ -76,27 +91,62 @@ document.addEventListener("DOMContentLoaded", function () {
     const loadDeskThree = new Promise((resolve) => { deskImgThree.onload = resolve; });
     const loadPrinter = new Promise((resolve) => { printerImg.onload = resolve; });
     const loadDocument = new Promise((resolve) => { documentImg.onload = resolve; });
+    const loadPersonOne = new Promise((resolve) => { personOne.onload = resolve; });
+    const loadPersonTwo = new Promise((resolve) => { personTwo.onload = resolve; });
+    const loadPersonThree = new Promise((resolve) => { personThree.onload = resolve; });
+    const loadCompOne = new Promise((resolve) => { compOne.onload = resolve; });
+    const loadCompTwo = new Promise((resolve) => { compTwo.onload = resolve; });
+    const loadCompThree = new Promise((resolve) => { compThree.onload = resolve; });
+    const loadCoffeeCup = new Promise((resolve) => { coffeeCup.onload = resolve; });
 
     // Wait for all images to load before starting the animation
-    Promise.all([loadFloor, loadDeskOne, loadDeskTwo, loadDeskThree, loadPrinter, loadDocument]).then(() => {
+    Promise.all([
+        loadFloor,
+        loadDeskOne,
+        loadDeskTwo,
+        loadDeskThree,
+        loadPrinter,
+        loadDocument,
+        loadPersonOne,
+        loadPersonTwo,
+        loadPersonThree,
+        loadCompOne,
+        loadCompTwo,
+        loadCompThree,
+        loadCoffeeCup
+    ]).then(() => {
         animate(); // Start the animation loop when all images are ready
     });
 
     // Positions and states
     const floorPos = { x: 0, y: 0 };
+    const comps = [
+        { x: 365, y: 420, img: compOne },
+        { x: 359, y: 90, img: compTwo },
+        { x: 620, y: 205, img: compThree },
+    ]
     const desks = [
         { x: 300, y: 450, img: deskImgOne },
         { x: 300, y: 100, img: deskImgTwo },
-        { x: 600, y: 150, img: deskImgThree }
+        { x: 600, y: 150, img: deskImgThree },
+    ];
+
+
+    const persons = [
+        { x: 354, y: 455, img: personOne },
+        { x: 359, y: 50, img: personTwo },
+        { x: 640, y: 170, img: personThree },
     ];
     const printerPos = { x: 50, y: 250 };
+    const coffeeCupPos = { x: 630, y: 270}
 
     // Main animation loop
     function animate() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         drawScene();
         animateJobs();
-        requestAnimationFrame(animate);
+       peopleTyping();
+        requestAnimationFrame(animate); 
     }
 
     // Function to draw the static parts of the scene
@@ -105,9 +155,78 @@ document.addEventListener("DOMContentLoaded", function () {
         desks.forEach(desk => {
             ctx.drawImage(desk.img, desk.x, desk.y);
         });
+        persons.forEach(person => {
+            ctx.drawImage(person.img, person.x, person.y);
+        });
+        comps.forEach(comp => {
+            ctx.drawImage(comp.img, comp.x, comp.y);
+        });
         ctx.drawImage(printerImg, printerPos.x, printerPos.y);
+        ctx.drawImage(coffeeCup, coffeeCupPos.x, coffeeCupPos.y)
     }
+    function peopleTyping() {
+        const typingAmplitude = 1; // Maximum pixel movement
+        const typingSpeedMin = 100; // Minimum speed of typing animation in milliseconds
+        const typingSpeedMax = 200; // Maximum speed of typing animation in milliseconds
+        const typingPauseMin = 500; // Minimum pause between typing bursts in milliseconds
+        const typingPauseMax = 2000; // Maximum pause between typing bursts in milliseconds
+    
+        // This loop will create a natural typing effect by moving the image up and down
+        persons.forEach(person => {
+            // Toggle the typing offset between -1 and 1 pixels to simulate a subtle typing effect
+            if (person.isTyping) {
+                person.typingOffset = person.typingOffset || 0;
+                person.typingDirection = person.typingDirection || 1;
+                person.typingOffset += person.typingDirection;
+                person.y += person.typingDirection; // Apply the offset to the y position
+    
+                // Change direction if amplitude is reached
+                if (Math.abs(person.typingOffset) > typingAmplitude) {
+                    person.typingDirection *= -1;
+                }
+            }
+        });
+    
+        // After altering the positions, redraw the scene to reflect the changes
+        drawScene();
+    
+        // Randomly decide to pause typing
+        if (Math.random() > 0.7) {
+            // Introduce a pause in typing
+            persons.forEach(person => {
+                person.isTyping = false;
+            });
+    
+            // Determine how long to pause before typing again
+            const typingPause = Math.floor(Math.random() * (typingPauseMax - typingPauseMin + 1) + typingPauseMin);
+            setTimeout(() => {
+                persons.forEach(person => {
+                    person.isTyping = true;
+                });
+                peopleTyping();
+            }, typingPause);
+        } else {
+            // Continue typing without pause
+            const typingSpeed = Math.floor(Math.random() * (typingSpeedMax - typingSpeedMin + 1) + typingSpeedMin);
+            setTimeout(peopleTyping, typingSpeed);
+        }
+    }
+    
+    // Initialize typing
+    persons.forEach(person => {
+        person.isTyping = true; // Start everyone typing
+    });
 
+
+     // Main animation loop
+     function animate() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        peopleTyping();
+        drawScene();
+        animateJobs();
+      
+        requestAnimationFrame(animate); 
+    }
     // Function to animate the documents moving to the printer
     function animateJobs() {
         printJobs.forEach(job => {
